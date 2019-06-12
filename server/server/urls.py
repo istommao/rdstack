69d493174698b5a0640171ad4072872263fa4ef8
istommao <istommao@gmail.com>
2019-06-12 21:37:37 +0800

    Add data json files

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,3 +1,5 @@
+"""coding:utf-8"""
+
 """server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,19 +15,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+import json
 import random
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+
 
 class IndexPageView(TemplateView):
     """IndexPageView"""
 
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):#向上下文变量中添加额外的数据
+    def get_context_data(self, **kwargs):
         context = super(IndexPageView, self).get_context_data(**kwargs)
+
+        fpath = os.path.join(settings.STATIC_ROOT, 'menulist.json')
+
+        with open(fpath, 'r') as out:
+            context['menu_list'] = json.load(out)
+
+        fpath = os.path.join(settings.STATIC_ROOT, 'datalist.json')
+        with open(fpath, 'r') as out:
+            context['data_list'] = json.load(out)
+
         context['randstr'] = random.randint(0, 10)
         return context
 
